42f2d0d2fac9d48a76c7aa40986c5861fe341735
Ryan W <wirer3@gmail.com>
2020-01-25 14:14:50 -0500

    Adding Hello World on 2 lines

---
+++ src/main/java/com/ryanw/console/HelloWorld2.java
@@ -0,0 +1,10 @@
+package com.ryanw.console;
+
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, ");
+        System.out.println("World");
+
+        System.out.println("Hello, \n\tWorld");
+    }
+}
